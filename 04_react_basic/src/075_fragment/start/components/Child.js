698a385e6541f084a1629e414133843de050c302
yuritakagi <takagiyuri0712@gmail.com>
2023-06-30 13:37:32 +0900

    add: 不要なタグを出力しないFragmentの使い方

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,21 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      {/* <></>で囲うとimportする必要もない */}
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+        repellat dolor doloribus iure consequatur soluta? Optio corrupti ratione
+        suscipit recusandae eius perspiciatis illo corporis? Aliquam nam
+        repellendus quos expedita est?
+        </p>
+    </React.Fragment>
   );
 };
 
