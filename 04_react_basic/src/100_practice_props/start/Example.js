e2c3d58d4892e7050e5ca10ffa203f2eabbf8f97
yuritakagi <takagiyuri0712@gmail.com>
2023-06-30 19:06:56 +0900

    add: 【練習】propsで値を渡してみよう

--- 04_react_basic/src/100_practice_props/start/Example.js
+++ 04_react_basic/src/100_practice_props/start/Example.js
@@ -3,21 +3,19 @@ import Profile from "./components/Profile";
 const profile = [
   { name: "Takashi", age: 19, country: "Japan" },
   { name: "Jane", age: 28, country: "UK" },
+  { name: "John Doe", age: "??", country: "Japan" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <h3>練習問題</h3>
-      <p>Profileコンポーネントの中身を変更して完成コードと同じ状態になるようにしてください。※なるべく分割代入を使用してください。
-      </p>
       <Profile
         name={profile[0].name}
         age={profile[0].age}
         country={profile[0].country}
       />
       <Profile {...profile[1]} />
-      <Profile />
+      <Profile {...profile[2]} />
     </div>
   );
 };
