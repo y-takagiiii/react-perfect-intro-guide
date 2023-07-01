f44956f89211470adce91f8adce5c035c802f2f6
yuritakagi <takagiyuri0712@gmail.com>
2023-07-02 08:51:17 +0900

    add: 41イベントに合わせて関数を実行してみよう

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,21 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert('ボタンがクリックされました。');
+  }
+  const clickHandler2 = () => {
+    console.log('ボタンがクリックされました');
+  }
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>クリックしてね</button>
+      <button onClick={clickHandler2}>クリックしてね</button>
+      {/* JSX内にJSを書きすぎると可読性下がる */}
+      {/* <button onClick={() => {
+        alert('アロー関数でもOK');
+      }}>
+        クリックしてね
+      </button> */}
+    </>
   );
 };
 
