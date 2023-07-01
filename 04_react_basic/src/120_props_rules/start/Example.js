64a84a96b9c0219d93c61997c8becc206b45e828
yuritakagi <takagiyuri0712@gmail.com>
2023-07-01 14:08:02 +0900

    add: propsの重要なルール

--- 04_react_basic/src/120_props_rules/start/Example.js
+++ 04_react_basic/src/120_props_rules/start/Example.js
@@ -1,14 +1,14 @@
-// POINT propsの流れは一方通行
-// POINT propsは読み取り専用
-
 import Bye from "./components/Bye"
 import Hello from "./components/Hello"
 
 const Example = () => {
+  // POINT propsの流れは一方通行
+  // 基本的に親コンポーネントから子コンポーネントに渡して使用する
+  const name = 'Tom';
   return (
     <>
-      <Hello />
-      <Bye />
+      <Hello name={name} />
+      <Bye name={name}/>
     </>
   );
 };
