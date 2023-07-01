82c2fde3b4bac8ffa9a01dc8b71dc4d93951a0f0
yuritakagi <takagiyuri0712@gmail.com>
2023-07-01 13:35:25 +0900

    add: 特別なプロパティ~props.children

--- 04_react_basic/src/110_props_children/start/components/Container.js
+++ 04_react_basic/src/110_props_children/start/components/Container.js
@@ -1,9 +1,13 @@
 import "./Container.css";
 
-const Container = ({ title }) => {
+const Container = ({ title, children, first, second }) => {
+  console.log(children);
   return (
     <div className="container">
       <h3>{title}</h3>
+      {children}
+      {second}
+      {first}
     </div>
   );
 };
