c51689cb8b3f612b70cb757c191f7f9e220c66e2
yuritakagi <takagiyuri0712@gmail.com>
2023-07-01 14:08:07 +0900

    add: propsの重要なルール

--- 04_react_basic/src/120_props_rules/start/components/Hello.js
+++ 04_react_basic/src/120_props_rules/start/components/Hello.js
@@ -1,4 +1,14 @@
 const Hello = (props) => {
+  // POINT propsは読み取り専用
+  const desc = Reflect.getOwnPropertyDescriptor(props, 'name');
+  console.log(desc);
+  // => Object
+  /*
+  { configurable: false  }
+  { enumerable: true }
+  { value: "Tom" }
+  { writable: false } 書き換え不可
+  */
   return (
     <div>
       <h3>Hello {props.name}</h3>
