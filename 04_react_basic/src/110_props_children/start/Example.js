88439dd46a743f106f4ce93bb4077406c2557e27
yuritakagi <takagiyuri0712@gmail.com>
2023-07-01 13:35:31 +0900

    add: 特別なプロパティ~props.children

--- 04_react_basic/src/110_props_children/start/Example.js
+++ 04_react_basic/src/110_props_children/start/Example.js
@@ -2,14 +2,27 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan", color: "blue" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      {/* <Container title="Childrenとは？">
+        <Profile {...profile[0]} />
+        <Profile {...profile[1]} />
+      </Container> */}
+      {/* childrenを属性として定義しても同じ */}
+      <Container title="Childrenとは？" children={
+        [
+          <Profile key={profile[0].name} {...profile[0]} />,
+          <Profile keu={profile[1].name} {...profile[1]} />
+        ]
+      }
+      first={<Profile key={profile[0].name} {...profile[0]} />}
+      second={<Profile key={profile[1].name} {...profile[1]}></Profile>}
+      />
     </div>
   );
 };
