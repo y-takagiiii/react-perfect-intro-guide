cd94ef9ca81cc50d3c7353d2dd2b25f7adbf1349
yuritakagi <takagiyuri0712@gmail.com>
2023-07-02 10:12:43 +0900

    add: イベントに合わせて画面表示を更新してみよう

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const Example = () => {
+  // let valArray = useState();
+
+  // useState 配列の0番目に参照用の値、1番目に更新用の関数が渡ってくる
+  // 分割代入を使う
+  let [val, setVal] = useState();
+  // console.log(valArray);
 
   return (
     <>
-      
+      <input
+        type="text"
+        onChange={(e) => {
+          // const setFn = valArray[1];
+          setVal(e.target.value)
+        }} /> = {val}
     </>
   );
 };
