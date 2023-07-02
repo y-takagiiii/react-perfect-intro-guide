0634343ed07c8eb121fb4feaeac4ee485c95a736
yuritakagi <takagiyuri0712@gmail.com>
2023-07-02 15:03:44 +0900

    add: ステートとコンポーネントの関係

--- 05_state_and_event/src/070_state_and_component/start/Example.js
+++ 05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,6 +1,25 @@
 import { useState } from "react";
 
 const Example = () => {
+  // とぢらのCountコンポーネントを表示するか切り替えるためのフラグを用意
+  const [ toggle, setToggle ] = useState(true);
+  const toggleComponent = () => {
+    setToggle(prev => !prev);
+  };
+  return (
+    <>
+      <button
+        onClick={toggleComponent}
+      >Toggle</button>
+      {/* このままだとReact要素のツリー構造が同じ位置にあるためstateを引き継いでしまう */}
+      {/* {toggle ? <Count title="A" /> : <Count title="B" />} */}
+      {/* keyをセットして解決 */}
+      {toggle ? <Count key="A" title="A" /> : <Count key="B" title="B" />}
+    </>
+  );
+};
+
+const Count = ({ title }) => {
   const [count, setCount] = useState(0);
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
@@ -10,7 +29,7 @@ const Example = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>{title}: {count}</h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
