6ece70cf56a7f43dae2558eee86cf1dceea370a6
yuritakagi <takagiyuri0712@gmail.com>
2023-07-02 14:42:44 +0900

    add: 配列のステートを使う際の注意点

--- 05_state_and_event/src/064_state_array/start/Example.js
+++ 05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+  // console.log(nums);
 
+  // shuffle関数で配列の最後の値を最初に持ってくる
+  const shuffle = () => {
+    // numsを新しい配列に変換
+    const newNums = [ ...nums ];
+    const value = newNums.pop();
+    newNums.unshift(value);
+    setNums(newNums);
+  }
   return (
     <>
-      
+      <h1>
+        { nums }
+      </h1>
+      <button
+        onClick={shuffle}
+      >shuffle</button>
     </>
   );
 };
