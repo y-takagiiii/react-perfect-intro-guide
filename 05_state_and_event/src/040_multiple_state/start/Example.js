850d1436821db0b600f1e771ee1beff9742e2b80
yuritakagi <takagiyuri0712@gmail.com>
2023-07-02 12:37:44 +0900

    add: 【複数のステート】ステート使用時の注意点！

--- 05_state_and_event/src/040_multiple_state/start/Example.js
+++ 05_state_and_event/src/040_multiple_state/start/Example.js
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const Example = () => {
-  
+  let [countA, setCountA] = useState(0);
+  let [countB, setCountB] = useState(10);
+  let [countC, setCountC] = useState(100);
+
+  return (
+    <>
+      <p>ボタンAを{countA}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountA(countA + 1);
+        }}
+      >ボタンA
+      </button>
+      <p>ボタンBを{countB}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountB(countB + 1);
+        }}
+      >ボタンB
+      </button>
+      <p>ボタンCを{countC}回押しました！</p>
+      <button
+        onClick={() => {
+          setCountC(countC + 1);
+        }}
+      >ボタンC
+      </button>
+    </>
+  );
 };
 
 export default Example;
