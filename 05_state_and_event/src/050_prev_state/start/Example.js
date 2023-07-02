b1e8dcacf1a34a9659795d04a06df9970ddeabf3
yuritakagi <takagiyuri0712@gmail.com>
2023-07-02 13:08:35 +0900

    add: ステート使用上の注意点

--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,45 @@
 import { useState } from "react";
 
+// const Example = () => {
+//   const [count, setCount] = useState(0);
+//   return (
+//     <>
+//       <p>現在のカウント数：{count}</p>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >+</button>
+//       <button
+//         onClick={() => {
+//           setCount(count - 1);
+//         }}
+//       >-</button>
+//     </>
+//   )
+// };
+
+// 名前付き関数で可読性を高める
 const Example = () => {
-  
+  const [count, setCount] = useState(0);
+  const countUp = () => {
+    setCount(count + 1);
+    setCount(prevstate => prevstate + 1);
+  };
+  const countDown = () => {
+    setCount(count - 1);
+  };
+  return (
+    <>
+      <p>現在のカウント数：{count}</p>
+      <button
+        onClick={countUp}
+      >+</button>
+      <button
+        onClick={countDown}
+      >-</button>
+    </>
+  )
 };
 
 export default Example;
