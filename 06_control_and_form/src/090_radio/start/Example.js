bba5d51a55a11d578b0e1b7ba4da0ea4a82e1852
yuritakagi <takagiyuri0712@gmail.com>
2023-07-03 18:58:28 +0900

    add: ラジオボタンの作成方法

--- 06_control_and_form/src/090_radio/start/Example.js
+++ 06_control_and_form/src/090_radio/start/Example.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+
 const Example = () => {
+
+  const [fruit, setFruit] = useState("");
+  const onChange = (e) => setFruit(e.target.value);
+  const RADIO_COLLECTION = ["Apple", "Banana", "Cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      {RADIO_COLLECTION.map(value => {
+        return (
+          <label key={value}>
+            <input
+              type="radio"
+              value={value}
+              checked={fruit === value}
+              onChange={onChange}
+            />
+          {value}
+        </label>
+        );
+      })}
+
+      <h3>私は{fruit}が食べたい</h3>
+    </>
   );
 };
 
