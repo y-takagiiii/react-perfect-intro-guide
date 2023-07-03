a6783b78c861e1edd29ed177362fb32a9f6a87f6
yuritakagi <takagiyuri0712@gmail.com>
2023-07-03 18:27:48 +0900

    add: inputとtextareaの作成方法

--- 06_control_and_form/src/080_input_textarea/start/Example.js
+++ 06_control_and_form/src/080_input_textarea/start/Example.js
@@ -1,10 +1,31 @@
+import { useState } from 'react';
+
 const Example = () => {
+
+  const [val, setVal] = useState("");
+  const clearVal = ()  => setVal("");
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <div>
+      {/* label for と同じ。forはJSでよく使われるキーワードなので、JSXではhtmlForとする */}
+      <label htmlFor="456">ラベル</label>
+      <div>
+        <input
+          id="123"
+          placeholder="こんにちは"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <textarea
+          id="456"
+          placeholder="こんにちは"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+      </div>
+      <h3>{val}</h3>
+      <button onClick={clearVal}>クリア</button>
+    </div>
   );
 };
 
