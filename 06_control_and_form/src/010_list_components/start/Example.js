07eb8c699faf3ae84e9d80aaa91f5ba8ec295d7a
yuritakagi <takagiyuri0712@gmail.com>
2023-07-03 12:35:09 +0900

    add: 配列をリスト表示

--- 06_control_and_form/src/010_list_components/start/Example.js
+++ 06_control_and_form/src/010_list_components/start/Example.js
@@ -2,12 +2,24 @@
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  // あまり使わない
+  // forは文なのでreturnの中(JSX)で使えない
+  // const animalList = [];
+  // for(const animal of animals) {
+  //   animalList.push(<li>{animal}</li>)
+  // }
+
+  // 一般的にはmapを使う
+  const helloAnimals = animals.map((animal) => <li>Hello, {animal}</li>);
 
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {/* {animalList} */}
+        {helloAnimals}
+        {/* 式はJSXとして扱える */}
+        {/* {animals.map((animal) => <li>Hello, {animal}</li>)} */}
       </ul>
     </>
   );
