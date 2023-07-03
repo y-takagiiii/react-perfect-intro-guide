e50b269de5f17066de7fbb2b182fe054137ce6a5
yuritakagi <takagiyuri0712@gmail.com>
2023-07-03 17:44:57 +0900

    ref: コンポーネントのリファクタリング

---
+++ 06_control_and_form/src/070_refactor_components/start/components/AnimalList.js
@@ -0,0 +1,20 @@
+import AnimalItem from './AnimalItem';
+
+const AnimalList = ({ animals }) => {
+  if(animals.length === 0) {
+    return <h3>アニマルが見つかりません。</h3>;
+  }
+
+  return (
+    <ul>
+      {animals
+        .map((animal) => {
+          return (
+            <AnimalItem key={animal} animal={animal} />
+          );
+        })}
+    </ul>
+  );
+};
+
+export default AnimalList;
