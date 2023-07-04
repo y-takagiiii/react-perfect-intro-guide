a576c016dba611425406cca0725fc8efd2b7b44e
yuritakagi <takagiyuri0712@gmail.com>
2023-07-04 16:18:07 +0900

    add: 【発展】CSS-in-JSを使ったスタイリング

--- 07_styling_component/src/040_css_in_js/start/Example.js
+++ 07_styling_component/src/040_css_in_js/start/Example.js
@@ -14,6 +14,25 @@ const StyledButton = styled.button`
   /* styled componentにはpropsも渡せる */
   /* propsを受け取りたい部分を関数にする */
   background: ${({ isSelected }) => isSelected ? 'pink' : ''};
+
+  /* メディアクエリも使用可能 */
+  /* 画面が600px以下のときはボタンの角を四角にする */
+  @media (max-width: 600px) {
+    border-radius: 0;
+  }
+`;
+
+// styled componentの継承
+const OrangeButton = styled(StyledButton)`
+  background-color: orange;
+  :hover {
+    color: red;
+    opacity: 0.7;
+  }
+/* ネスト記法も使える */
+  span {
+    font-size: 2em;
+  }
 `;
 
 const Example = () => {
@@ -24,6 +43,7 @@ const Example = () => {
   return (
     <>
       <StyledButton isSelected={isSelected} onClick={clickHandler} >ボタン</StyledButton>
+      <OrangeButton isSelected={isSelected} onClick={clickHandler} ><span>ボタン</span></OrangeButton>
       <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
         ボタン
       </button>
