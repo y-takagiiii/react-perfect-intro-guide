e575eca43cb25b597895c50c88034a2676385f64
yuritakagi <takagiyuri0712@gmail.com>
2023-07-04 09:57:51 +0900

    add: 【Form】プルダウンの作成方法

--- 06_control_and_form/src/120_select/start/Example.js
+++ 06_control_and_form/src/120_select/start/Example.js
@@ -1,10 +1,23 @@
+import { useState } from 'react'
+
 const Example = () => {
+  const [selected, setSelected] = useState("Banana");
+  const OPTIONS = ["Apple", "Banana", "Cherry"];
+
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <select
+        value={selected}
+        onChange={e => setSelected(e.target.value)}
+      >
+        {/* <option value="Apple">Apple</option>
+        <option value="Banana">Banana</option>
+        <option value="Cherry">Cherry</option> */}
+        {/* mapで書き換え */}
+        {OPTIONS.map(opt => <option key={opt} value={opt} >{opt}</option>)}
+      </select>
+      <div>選択された果物:{selected}</div>
+    </>
   );
 };
 
