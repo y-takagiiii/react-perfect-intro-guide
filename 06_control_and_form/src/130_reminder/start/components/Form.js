f6dcd4e73a20aca5296ab2a29528f7bc7d9a1058
yuritakagi <takagiyuri0712@gmail.com>
2023-07-04 11:28:58 +0900

    add: Todoアプリを作ってみよう

---
+++ 06_control_and_form/src/130_reminder/start/components/Form.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const Form = ({createTodo}) => {
+  const [enteredTodo, setEnteredTodo] = useState("");
+
+  const addTodo = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      id: Math.floor(Math.random() * 1e5),
+      content: enteredTodo
+    };
+    createTodo(newTodo);
+    setEnteredTodo("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={addTodo}>
+        <input type="text" onChange={(e) => setEnteredTodo(e.target.value)} />
+        <button>追加</button>
+      </form>
+    </div>
+  );
+}
+
+export default Form;
