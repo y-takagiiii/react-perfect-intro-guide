6467685d82c68ec8804eb71615b4b6c50f656f7c
yuritakagi <takagiyuri0712@gmail.com>
2023-07-04 16:07:10 +0900

    add: 【styled-components】CSS-in-JSを使ったスタイリング

--- 07_styling_component/src/040_css_in_js/start/Example.js
+++ 07_styling_component/src/040_css_in_js/start/Example.js
@@ -1,4 +1,20 @@
 import { useState } from "react";
+import styled from "styled-components";
+console.dir(styled);
+const StyledButton = styled.button`
+/* タグ付きテンプレート ^^^^^^^^^^^^^^^ */
+  margin: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  font-weight: bold;
+  cursor: pointer;
+  /* styled componentにはpropsも渡せる */
+  /* propsを受け取りたい部分を関数にする */
+  background: ${({ isSelected }) => isSelected ? 'pink' : ''};
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -7,6 +23,7 @@ const Example = () => {
 
   return (
     <>
+      <StyledButton isSelected={isSelected} onClick={clickHandler} >ボタン</StyledButton>
       <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
         ボタン
       </button>
