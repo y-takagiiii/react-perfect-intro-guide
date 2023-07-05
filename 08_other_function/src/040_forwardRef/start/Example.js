5f6497656659b164fdb6ccae8287ce7e867fcce0
yuritakagi <takagiyuri0712@gmail.com>
2023-07-05 14:36:06 +0900

    add: 【forwardRef】他のコンポーネントのDOMにアクセスする方法

--- 08_other_function/src/040_forwardRef/start/Example.js
+++ 08_other_function/src/040_forwardRef/start/Example.js
@@ -1,10 +1,18 @@
 import { useRef, forwardRef } from "react";
 
+// propsでrefは渡せない
+// forwardRefの引数として関数コンポーネントを渡す
+// ただし、別のコンポーネントにrefを渡すことは依存関係が複雑になるので、なるべく避ける
+const Input = forwardRef((props, ref) => {
+  return <input type="text" ref={ref} />
+});
+
 const Example = () => {
   const ref = useRef();
   return (
     <>
-      <input type="text" ref={ref} />
+      {/* Exampleコンポーネント内で定義したrefをInputコンポーネントに渡す */}
+      <Input ref={ref} />
       <button onClick={() => ref.current.focus()}>
         インプット要素をフォーカスする
       </button>
