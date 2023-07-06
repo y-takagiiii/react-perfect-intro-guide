693d5e9674a2ef5302b3e521fe39b5031485d027
yuritakagi <takagiyuri0712@gmail.com>
2023-07-06 20:56:31 +0900

    add: デバッガーを使ってみよう

--- 09_debugging/src/020_debugging/start/Example.js
+++ 09_debugging/src/020_debugging/start/Example.js
@@ -1,13 +1,15 @@
 import { useState } from "react"
 
-const Child = (countNum) => {
-  return <p>現在のカウント数: {countNum.val}</p>
+const Child = ({ count }) => {
+  // debugger 
+  return <p>現在のカウント数: {count.val}</p>
 }
 
 const Example = () => {
   const [count, setCount] = useState({ val: 0 });
 
   const countUp = () => {
+    // debugger
     setCount((prevstate) => {
       const newState = { val: prevstate.val + 1 }
       return newState;
