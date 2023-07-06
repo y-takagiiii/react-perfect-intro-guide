9049f062b261ce0a38e8486c8b7021d3ecd89d39
yuritakagi <takagiyuri0712@gmail.com>
2023-07-06 20:04:21 +0900

    add: デバッガーを使ってみよう

--- 09_debugging/src/010_check_error/start/Example.js
+++ 09_debugging/src/010_check_error/start/Example.js
@@ -1,17 +1,28 @@
+import { useState } from 'react';
+
 const Example = () => {
-  // const [count, setCount] = useState({ val: 0 });
+  const [count, setCount] = useState({ val: 0 });
 
-  // const countUp = () => {
-  //   setCount((prevstate) => {
-  //     prevstate.val += 1;
-  //     return prevstate;
-  //   });
-  // };
+  // console.log('リレンダリング'); // => +ボタンを押したときに、リレンダリングされていないことが分かる
 
-  // return (
-  //     <p>現在のカウント数: {count}</p>
-  //     <button onClick={countUp}>+</button>
-  // );
+  const countUp = () => {
+    setCount((prevstate) => {
+      // const newState = { ...prevstate }
+      // newState.val += 1;
+
+      // プロパティがval1つだけなので、わざわざスプレッド演算子で展開してあげなくてもいい
+      const newState = { val: prevstate.val + 1 };
+      newState.val += 1;
+      return newState;
+    });
+  };
+
+  return (
+    <>
+      <p>現在のカウント数: {count.val}</p>
+      <button onClick={countUp}>+</button>
+    </>
+  );
 };
 
 export default Example;
