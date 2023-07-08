7bbeb3cc3abd54b3b4b64c1726d196ddc2a9bd72
yuritakagi <takagiyuri0712@gmail.com>
2023-07-08 17:43:27 +0900

    add: 指定されたパスの値をJSで取得してみよう

--- 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
@@ -1,3 +1,35 @@
-export default function Setting () {
-  return <h1>[name]/setting.js</h1>
+// Next.jsの機能を使う方法
+export default function Setting ({ query }) {
+  return <h1>nextで取得 {query.name}</h1>
 }
+
+// getServerSideProps関数を使ってパスの情報を取得する
+export async function getServerSideProps(context) {
+  // console.log(context.query);
+  const { query } = context;
+  return {
+    props: { query }
+  }
+}
+
+// 分割代入で書き換え
+// export async function getServerSideProps({ query }) {
+//   return {
+//     props: { query }
+//   }
+// }
+
+// React HooksのuseRouterを使う方法
+// import { useRouter } from 'next/router'
+
+// export default function Setting ({ query }) {
+//   // トップレベルでrouterを宣言
+//   const router = useRouter();
+//   return <h1>routerから取得 {router.query.name}</h1>
+// }
+
+// export async function getServerSideProps({ query }) {
+//   return {
+//     props: { query }
+//   }
+// }
