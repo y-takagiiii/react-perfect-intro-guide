b7ca407434435471368919d5758829288591f7fe
yuritakagi <takagiyuri0712@gmail.com>
2023-07-08 15:59:23 +0900

    add: 動的なルーティング

---
+++ 17_nextjs_p1/start/src/pages/07_router/[name]/setting.js
@@ -0,0 +1,3 @@
+export default function Setting () {
+  return <h1>[name]/setting.js</h1>
+}
---
+++ 17_nextjs_p1/start/src/pages/07_router/blog/[number].js
@@ -0,0 +1,6 @@
+// [〇〇].jsとすると動的な(ダイナミック)ルーティングとなる
+// ダイナミックルーティングは一つの階層に一つだけしか設定できない
+
+export default function Number () {
+  return <h1>[number].js </h1>
+}
