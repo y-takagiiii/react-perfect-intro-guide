c9ae4912988d02cc3ceda0a67132151c1f7a1692
yuritakagi <takagiyuri0712@gmail.com>
2023-07-09 19:46:40 +0900

    add: useContextでstateを管理してみよう

--- 11_hooks_p1/src/040_useContext_with_state/start/Example.js
+++ 11_hooks_p1/src/040_useContext_with_state/start/Example.js
@@ -1,14 +1,16 @@
-import { createContext } from "react";
+  import { createContext, useState } from "react";
 import Child from "./components/Child";
 import OtherChild from "./components/OtherChild";
-export const MyContext = createContext("hello");
+export const MyContext = createContext();
 
 const Example = () => {
+  const [ state, setState ] = useState(0);
+
   return (
-    <>
+    <MyContext.Provider value={[ state, setState ]}>
       <Child />
       <OtherChild />
-    </>
+    </MyContext.Provider>
   );
 };
 
--- 11_hooks_p1/src/040_useContext_with_state/start/components/GrandChild.js
+++ 11_hooks_p1/src/040_useContext_with_state/start/components/GrandChild.js
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import { MyContext } from "../Example";
 const GrandChild = () => {
-  const value = useContext(MyContext);
+  const [state] = useContext(MyContext);
   return (
     <div style={{ border: "1px solid black" }}>
       <h3>孫コンポーネント</h3>
-      {value}
+      {state}
     </div>
   );
 };
