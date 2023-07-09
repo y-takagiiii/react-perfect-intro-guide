5f0c9c012f9d87000351d1cb046ff2347f0cfcd8
yuritakagi <takagiyuri0712@gmail.com>
2023-07-09 19:22:26 +0900

    add: useContextでグローバルな値を管理しよう

--- 11_hooks_p1/src/030_useContext/start/Example.js
+++ 11_hooks_p1/src/030_useContext/start/Example.js
@@ -1,8 +1,14 @@
 import Child from "./components/Child";
+import { createContext } from 'react';
 
+// コンテキスト作成
+export const MyContext = createContext('hello');
+
+// ExampleコンポーネントからGrandChildコンポーネントへpropsを渡すときはExample→Child→GrandChildと渡す必要がある(propsのバケツリレー)が
+// useContextを使うとExample→GrandChildと渡すことができる
 const Example = () => {
   const value = 'hello'
-  return <Child value={value}/>;
+  return <Child />;
 };
 
 export default Example;
--- 11_hooks_p1/src/030_useContext/start/components/GrandChild.js
+++ 11_hooks_p1/src/030_useContext/start/components/GrandChild.js
@@ -1,4 +1,10 @@
-const GrandChild = ({ value }) => {
+import { useContext } from 'react';
+import { MyContext } from '../Example';
+
+const GrandChild = () => {
+  // useContextで受け取る
+  const value = useContext(MyContext)
+
   return (
       <div style={{ border: "1px solid black" }}>
         <h3>孫コンポーネント</h3>
