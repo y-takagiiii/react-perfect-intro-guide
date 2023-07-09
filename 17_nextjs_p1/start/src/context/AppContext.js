bb44a51e03d4dfb811136b4bd779fe1d864eb355
yuritakagi <takagiyuri0712@gmail.com>
2023-07-09 14:55:52 +0900

    add: コンポーネント間で状態を共有する方法

---
+++ 17_nextjs_p1/start/src/context/AppContext.js
@@ -0,0 +1,17 @@
+import { useContext, createContext, useState } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [ list, setList ] = useState([]);
+
+  return (
+    <AppContext.Provider value={[list, setList]}>
+      { children }
+    </AppContext.Provider>
+  )
+}
+
+const useAppContext = () => useContext(AppContext);
+
+export { AppProvider, useAppContext }
