1fe5b39991ee2c5ef511391d6b1d9ceff1be5bf5
yuritakagi <takagiyuri0712@gmail.com>
2023-07-11 12:24:18 +0900

    add: useContextのリファクタリングをしてみよう

--- 11_hooks_p1/src/050_context_file/start/Example.js
+++ 11_hooks_p1/src/050_context_file/start/Example.js
@@ -1,22 +1,15 @@
-import { useState } from "react";
 import "./Example.css";
+import Main from "./components/Main"
+import Header from "./components/Header"
+import { ThemeProvider } from './context/ThemeContext'
 
 const Example = () => {
-  const [theme, setTheme] = useState('light')
-  
-  const changeTheme = (e) => setTheme(e.target.value)
-  
-  const THEMES = ['light', 'dark', 'red'];
 
   return (
-    <>
-      <header className={`content-${theme}`}>
-        
-      </header>
-      <main className={`content-${theme}`}>
-        <h1>テーマの切り替え</h1>
-      </main>
-    </>
+    <ThemeProvider>
+      <Header />
+      <Main />
+    </ThemeProvider>
   );
 };
 
