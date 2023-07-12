64d94544fed9774f9b53fa4982208775e837b6ff
yuritakagi <takagiyuri0712@gmail.com>
2023-07-12 22:38:44 +0900

    add: useContextを使う際の注意点

--- 11_hooks_p1/src/050_context_file/start/context/ThemeContext.js
+++ 11_hooks_p1/src/050_context_file/start/context/ThemeContext.js
@@ -1,15 +1,20 @@
 import { useState, useContext, createContext } from "react";
 
 export const ThemeContext = createContext();
+export const ThemeUpdateContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("light");
 
   return (
-    <ThemeContext.Provider value={[theme, setTheme]}>
-      {children}
+    // 不要なレンダリングを避けるために、stateと更新用関数を分けて子要素に渡す
+    <ThemeContext.Provider value={theme}>
+      <ThemeUpdateContext.Provider value={setTheme}>
+        {children}
+      </ThemeUpdateContext.Provider>
     </ThemeContext.Provider>
   )
 }
 
 export const useTheme = () => useContext(ThemeContext);
+export const useUpdateTheme = () => useContext(ThemeUpdateContext);
