794ea22e12fd5d7b026d8a8ba0d4196d969b9179
yuritakagi <takagiyuri0712@gmail.com>
2023-07-20 18:59:31 +0900

    add: 状態と処理の分離

--- 10_functional_programming/src/010_data_procedure/start/Example.js
+++ 10_functional_programming/src/010_data_procedure/start/Example.js
@@ -1,8 +1,39 @@
+// 関数型プログラミング
+// (値の)状態と処理を分離して管理
+// A(data) -> B(data) -> C(data) -> 結果
+
+// オブジェクト指向型プログラミング
+// 状態と処理を対で管理
+// object.method(); -> 結果
+
 const Example = () => {
+  // オブジェクト指向型 データ(nums)と処理(sum())を対で管理しておく
+  const numObj = {
+    nums: [1, 2, 3],
+    sum() {
+      const nums = this.nums;
+      let sumValue = 0;
+      for(let i = 0; i < nums.length; i++) {
+        sumValue += nums[i];
+      }
+      return sumValue;
+    }
+  };
+
+  // 関数型
+  const nums = [1, 2, 3];
+  const sum = (array) => {
+    const nums = array;
+    let sumValue = 0;
+    for(let i = 0; i < nums.length; i++) {
+      sumValue += nums[i];
+    }
+    return sumValue;
+  }
   return (
     <>
-      <h3>状態管理と処理を分離</h3>
-      <p>状態（データ）と処理（やりたいこと）は切り離す</p>
+      <div>オブジェクト指向型：{numObj.sum()}</div>
+      <div>関数型：{sum(nums)}</div>
     </>
   );
 };
