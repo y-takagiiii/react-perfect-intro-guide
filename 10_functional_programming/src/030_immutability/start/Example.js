4104c8f54812ec7593267fc615d0fadbdede10a6
yuritakagi <takagiyuri0712@gmail.com>
2023-07-20 20:49:05 +0900

    add: 不変性part1

--- 10_functional_programming/src/030_immutability/start/Example.js
+++ 10_functional_programming/src/030_immutability/start/Example.js
@@ -1,4 +1,13 @@
 const Example = () => {
+  const num = { val2: 2 }
+  const double = (num) => {
+    const newNum = { val: num.val * 2 };
+    // newNum.val = num.val * 2;
+    return newNum;
+  }
+  const newNum = double(num);
+  console.log('newNum', newNum, 'num', num); 
+
   return (
     <>
       <h3>不変性（Immutability）</h3>
