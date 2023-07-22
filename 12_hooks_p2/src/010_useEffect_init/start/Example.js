d66d6703e274040c4d001335c89f848e331aa2c8
yuritakagi <takagiyuri0712@gmail.com>
2023-07-22 17:55:54 +0900

    add: useEffectとは？

--- 12_hooks_p2/src/010_useEffect_init/start/Example.js
+++ 12_hooks_p2/src/010_useEffect_init/start/Example.js
@@ -1,5 +1,28 @@
+import { useState, useEffect } from 'react'
+
 const Example = () => {
-  return <></>;
+  const [time, setTime] = useState(0);
+
+  // 再レンダリングされるたびにsetTimeが実行されるのでタイマーがバグる
+  // window.setInterval(() => {
+    //   setTime(prev => prev + 1);
+    // }, 1000)
+
+    // 解決策としてuseEffectを使う
+  useEffect(() => {
+    window.setInterval(() => {
+      setTime(prev => prev + 1);
+    }, 1000)
+  }, [])
+
+  return (
+    <>
+      <h3>
+        <time>{time}</time>
+        <span>秒経過</span>
+      </h3>
+    </>
+  );
 };
 
 export default Example;
