0af03387224a937c5ca44f5a39545520b326823c
yuritakagi <takagiyuri0712@gmail.com>
2023-07-23 14:00:37 +0900

    useEffectの依存配列の使い方

--- 12_hooks_p2/src/020_useEffect_update/start/Example.js
+++ 12_hooks_p2/src/020_useEffect_update/start/Example.js
@@ -9,7 +9,20 @@ const Example = () => {
       setTime(prev => prev + 1);
     }, 1000);
   }, [])
-  
+
+  // useEffectの第二引数[]は依存配列と呼ばれる
+  // 依存配列に含めたstateが更新されると第一引数のコールバック関数が実行される -> timeが更新されるとupdatedが出力される
+  // 依存配列が空の場合はExampleコンポーネントが生成されたときのみコールバック関数が実行される
+  // useEffect(() => {
+  //   console.log('updated');
+  // }, [time])
+
+  // 1秒ごとにページのタイトルを変更し、ローカルストレージにキーを保存する
+  useEffect(() => {
+    document.title = 'counter:' + time;
+    window.localStorage.setItem('time-key', time);
+  }, [time])
+
   return (
     <h3>
       <time>{time}</time>
