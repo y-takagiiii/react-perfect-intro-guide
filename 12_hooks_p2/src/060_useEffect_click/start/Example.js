b50fea738053940f226f29d6aabf7b796620f650
yuritakagi <takagiyuri0712@gmail.com>
2023-07-27 22:12:28 +0900

    add: useEffectの実行順を意識して実装

--- 12_hooks_p2/src/060_useEffect_click/start/Example.js
+++ 12_hooks_p2/src/060_useEffect_click/start/Example.js
@@ -6,30 +6,33 @@ const Example = () => {
   return (
     <>
       {isDisp && <Timer/>}
-      <button onClick={() => setIsDisp(prev => !prev)}>トグル</button>
+      <button onClick={() => setIsDisp(prev => !prev)}>{isDisp ? '非表示' : '表示'}</button>
     </>
   )
-}
+} 
 
 const Timer = () => {
   const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
     // console.log('init');
     let intervalId = null;
-    intervalId = window.setInterval(() => {
-      // console.log('interval running');
-      setTime(prev => prev + 1);
-    }, 1000);
+    if(isRunning) {
+      intervalId = window.setInterval(() => {
+        // console.log('interval running');
+        setTime(prev => prev + 1);
+      }, 1000);
+    }
     return () => {
       window.clearInterval(intervalId)
       // console.log('end');
     }
-  }, [])
-  
+  }, [isRunning])
+
   useEffect(() => {
     // console.log('updated');
-    
+
     document.title = 'counter:' + time;
     window.localStorage.setItem('time-key', time);
 
@@ -46,11 +49,26 @@ const Timer = () => {
     }
   }, [])
 
+  const toggle = () => {
+    setIsRunning(prev => !prev);
+  }
+
+  const reset = () => {
+    setIsRunning(false);
+    setTime(0);
+  }
+
   return (
-    <h3>
-      <time>{time}</time>
-      <span>秒経過</span>
-    </h3>
+    <>
+      <h3>
+        <time>{time}</time>
+        <span>秒経過</span>
+      </h3>
+      <div>
+        <button onClick={toggle}>{isRunning ? '一時停止' : 'スタート'}</button>
+        <button onClick={reset}>リセット</button>
+      </div>
+    </>
     );
 };
 
