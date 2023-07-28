51fffaa54aff25fc3658c09ca56431056a8c1c31
yuritakagi <takagiyuri0712@gmail.com>
2023-07-28 09:13:26 +0900

    add: カスタムフック

--- 12_hooks_p2/src/070_customHook/start/Example.js
+++ 12_hooks_p2/src/070_customHook/start/Example.js
@@ -1,5 +1,7 @@
 import { useEffect, useState, useLayoutEffect } from "react";
 
+import useTimer from "./useTimer";
+
 const Example = () => {
   const [isDisp, setIsDisp] = useState(true);
 
@@ -12,55 +14,8 @@ const Example = () => {
 }
 
 const Timer = () => {
-  const [time, setTime] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
-
-  useEffect(() => {
-    // console.log('init');
-    let intervalId = null;
-
-    if(isRunning) {
-      // console.log('timer start');
-
-      intervalId = window.setInterval(() => {
-        // console.log('interval running');
-        setTime(prev => prev + 1);
-      }, 1000);
-    }
-    
-    return () => {
-      window.clearInterval(intervalId)
-      // console.log('end');
-    }
-  }, [isRunning])
-  
-  useEffect(() => {
-    // // console.log('updated');
-    
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key', time);
-
-    return () => {
-      // debugger
-      // // console.log('updated end');
-    }
-  }, [time]);
-
-  useLayoutEffect(() => {
-    const _time = parseInt(window.localStorage.getItem('time-key'));
-    if(!isNaN(_time)) {
-      setTime(_time);
-    }
-  }, [])
-
-  const toggle = () => {
-    setIsRunning(prev => !prev);
-  }
-
-  const reset = () => {
-    setTime(0);
-    setIsRunning(false);
-  }
+  // 分割代入でuseTimer.jsから送られた変数・関数を受け取る
+  const { toggle, reset, time, isRunning } = useTimer();
 
   return (
     <>
---
+++ 12_hooks_p2/src/070_customHook/start/useTimer.js
@@ -0,0 +1,65 @@
+import { useState, useEffect, useLayoutEffect } from 'react';
+
+// カスタムフック
+const useTimer = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    // console.log('init');
+    let intervalId = null;
+
+    if(isRunning) {
+      // console.log('timer start');
+
+      intervalId = window.setInterval(() => {
+        // console.log('interval running');
+        setTime(prev => prev + 1);
+      }, 1000);
+    }
+
+    return () => {
+      window.clearInterval(intervalId)
+      // console.log('end');
+    }
+  }, [isRunning])
+
+  useEffect(() => {
+    // // console.log('updated');
+
+    document.title = 'counter:' + time;
+    window.localStorage.setItem('time-key', time);
+
+    return () => {
+      // debugger
+      // // console.log('updated end');
+    }
+  }, [time]);
+
+  useLayoutEffect(() => {
+    const _time = parseInt(window.localStorage.getItem('time-key'));
+    if(!isNaN(_time)) {
+      setTime(_time);
+    }
+  }, [])
+
+  const toggle = () => {
+    setIsRunning(prev => !prev);
+  }
+
+  const reset = () => {
+    setTime(0);
+    setIsRunning(false);
+  }
+
+  // Example.jsへ使いたい変数・関数を渡す
+  return {
+    // toggle: toggleの省略
+    toggle,
+    reset,
+    time,
+    isRunning,
+  }
+}
+
+export default useTimer;
