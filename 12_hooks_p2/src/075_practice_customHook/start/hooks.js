91ab9b05f0790852f52265e770fe894dedbb94ba
yuritakagi <takagiyuri0712@gmail.com>
2023-07-31 19:54:20 +0900

    add: 【練習】カスタムフック

---
+++ 12_hooks_p2/src/075_practice_customHook/start/hooks.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export const useCount = () => {
+  const [count, setCount] = useState(0);
+  const countUp = () => {
+    setCount(count + 1)
+  };
+
+  return { count, countUp };
+};
