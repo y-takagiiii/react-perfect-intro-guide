3801e987f33ae83fb0929bb9c28b4896e609bdb2
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-18 18:40:02 +0900

    ユニオン型の定義方法

--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,30 @@
 const Example = () => {
   // 練習用
+  let strOrNum: string | number = 'Hello'
+  strOrNum = 123
+  console.log(strOrNum)
+
+  let strOrNumOrBool: string | number | boolean = 'Hello'
+  strOrNumOrBool = false
+  console.log(false)
+
+  let helloOrNumOrBoolOr: 'Hello' | number | boolean = 'Hello'
+  // helloOrNumOrBoolOr = 'Bye' // 'Hello'、数値、真偽値しか代入できない
+  console.log(helloOrNumOrBoolOr)
+
+  // 変数宣言前に型を定義しておく
+  type HelloOrNum = 'Hello' | number;
+  const hello: HelloOrNum = 'Hello';
+
+  type DayOfWeek =
+    | 'Monday' // 見栄えのために意味のないパイプを書くこともある
+    | 'Tuesday'
+    | 'Wednesday'
+    | 'Thursday'
+    | 'Friday'
+    | 'Saturday'
+    | 'Sunday'
+  const day: DayOfWeek = 'Friday'
 };
 
 export default Example;
