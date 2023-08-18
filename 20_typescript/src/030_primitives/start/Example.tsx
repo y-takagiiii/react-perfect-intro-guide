75a7b89b6748fa747d0246066fd55fe9aa592ea7
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-18 17:57:36 +0900

    プリミティブな値の型定義

--- 20_typescript/src/030_primitives/start/Example.tsx
+++ 20_typescript/src/030_primitives/start/Example.tsx
@@ -1,5 +1,32 @@
+// プリミティブとはTypeScriptが扱うことのできる基本的な値のこと
+// 文字列、数値、巨大な数値、真偽値、null、undefinedなど
+
 const Example = () => {
   // 練習用
+  let str: string = 'Hello';
+  // str = 1; // 変数strにstring型以外の値は代入できない
+  str = "Bye";
+  console.log(str);
+
+  let num: number = 102;
+  console.log(num);
+
+  let bignum: bigint = 103n;
+  console.log(bignum);
+
+  let bool: boolean = false;
+  console.log(bool)
+
+  let nullish: null = null;
+  console.log(nullish)
+
+  let undefinedValue: undefined = undefined;
+  console.log(undefinedValue)
+
+  // リテラル型
+  let trueVal: true = true; // trueValにはtrueしか代入できない
+  let num123: 123 = 123; // num113には123しか代入できない
+  let strHello: 'Hello' = 'Hello'
 };
 
 export default Example;
