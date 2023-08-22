fea52e2a5ed0cfbca771f34bf7b654cf5013b0ab
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-22 12:22:47 +0900

    関数コンポーネントに型を導入

--- 20_typescript/src/110_component/start/Example.tsx
+++ 20_typescript/src/110_component/start/Example.tsx
@@ -1,5 +1,14 @@
-const Example = () => {
-    // 練習
-};
+// 関数コンポーネントに型を導入する
+// const Example: React. FC = () => {
+//     return <h1>Hello TypeScript</h1>
+// };
+
+// export default Example;
+
+import Hello from "./Hello";
+
+const Example: React.FC = () => {
+    return <Hello />
+}
 
 export default Example;
