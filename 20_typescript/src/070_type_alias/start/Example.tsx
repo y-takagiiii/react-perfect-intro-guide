979e00574eb734ca0809006a2b2e41ee6dc3321d
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-22 10:26:31 +0900

    型エイリアス

--- 20_typescript/src/070_type_alias/start/Example.tsx
+++ 20_typescript/src/070_type_alias/start/Example.tsx
@@ -1,5 +1,27 @@
 const Example = () => {
-  // 練習用
+  // TypeScriptでは、type文を用いて型に別名をつけることができる。
+  // これを型エイリアス(type alias)と呼ぶ
+  // 型エイリアスはパスカルケースで定義する
+  type User = {
+    name: string,
+    age: number
+  }
+  const user: User = { name: 'Taro', age: 10 }
+
+  type UserName = string;
+  type UserAge = number;
+  type UserGender = "man" | "woman" | "other";
+  type UserProfile = {
+    name: UserName,
+    age: UserAge,
+    gender: UserGender,
+  };
+
+  const userProfile: UserProfile = {
+    name: 'Hanako',
+    age: 21,
+    gender: 'woman'
+  }
 };
 
 export default Example;
