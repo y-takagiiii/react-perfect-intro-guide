e9defed541df927292559a35d7f3279216fee2a9
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-22 14:14:54 +0900

    classの型定義

--- 20_typescript/src/130_class/start/Example.tsx
+++ 20_typescript/src/130_class/start/Example.tsx
@@ -1,5 +1,25 @@
+class User {
+  public name: string;
+  private age: number;
+
+  constructor(name: string, age: number  ) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
 const Example = () => {
-  // 練習用
+  const user = { name: "Hanako", age: 21 };
+  const user1 = new User("Taro", 22)
+  console.log(user1)
+  // privateメソッドにしたプロパティはクラス外からアクセス不可
+  // console.log(user1.age)
+  return (
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+    </div>
+  )
 };
 
 export default Example;
