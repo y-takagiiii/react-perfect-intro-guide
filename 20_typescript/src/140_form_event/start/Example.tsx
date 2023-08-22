db87f860a775b44da97a5be153a676055fe8bd70
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-22 15:04:51 +0900

    useStateの型定義

--- 20_typescript/src/140_form_event/start/Example.tsx
+++ 20_typescript/src/140_form_event/start/Example.tsx
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+
+type User = {
+  name: string,
+  age: number
+}
+
+type Todo = {
+  id: number,
+  text: string
+}
+
 const Example = () => {
-  // 練習用
+  // 型推論が使える
+  const [ text, setText ] = useState("hello");
+  // 明示的に型を定義することもできる
+  const [ animals, setAnimals ] = useState<string[]>(["dog", "cat"]);
+  const [ users, setUsers ] = useState<User[]>([{name: 'Tom', age: 22}])
+
+  const [ todos, setTodos ] = useState<Todo[]>([{id: 1, text: 'homework'}])
 };
 
 export default Example;
