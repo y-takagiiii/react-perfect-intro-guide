5774c117ee637064a8f4bba23c5faa9c703492b4
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-22 10:12:50 +0900

    型推論とは

--- 20_typescript/src/060_type_inference/start/Example.tsx
+++ 20_typescript/src/060_type_inference/start/Example.tsx
@@ -1,5 +1,18 @@
 const Example = () => {
-  // 練習用
+  // TypeScriptでは型推論(type inference)によって型をある程度推論してくれる機能がある
+  // 基本亭には明らかに型がわかるような場合の型定義は型推論に任せるようにする
+
+  // let str: string = 'hello';
+  // 上記のように明示しなくても型定義してくれる
+  let str = 'hello';
+  // str = 123; // エラーになる
+
+  // オブジェクトもある程度型推論してくれる
+  let obj = { name: 'Taro', age: 10 };
+
+  // constで定義した変数の型はリテラル型になる
+  const bye = "bye";
+  const num = 123;
 };
 
 export default Example;
