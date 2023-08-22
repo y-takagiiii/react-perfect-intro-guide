aabc7c6b73596b4c5fe9a3e31769539307b3f14d
y-takagiiii <takagiyuri0712@gmail.com>
2023-08-22 12:05:24 +0900

    ジェネリクス

--- 20_typescript/src/090_generics/start/Example.ts
+++ 20_typescript/src/090_generics/start/Example.ts
@@ -1,5 +1,26 @@
 const Example = () => {
-  // 練習用
+  // ジェネリクスとは、型引数(type parameters)を受け取る関数を作る機能のことを指す
+  // 文字列を格納する配列を作る関数
+  const repeatStr = (value: string, times: number): string[] => {
+    return new Array(times).fill(value);
+  }
+  const strArray = repeatStr("hello", 3);
+  console.log(strArray); // => ['hello', 'hello', 'hello']
+
+  const repeatNum = (value: number, times: number): number[] => {
+    return new Array(times).fill(value);
+  }
+  const numArray = repeatNum(8, 3);
+  console.log(numArray); // => [8, 8, 8]
+
+  // ジェネリクスを使って型の安全性と保ちつつコードの共通化を図ることができる
+  const repeat = <T>(value: T, times: number): T[] => { // <T, U>のように複数渡すこともできる
+    return new Array(times).fill(value);
+  }
+  const genericsNumArray = repeat<number>(10, 4); // Tの部分がnumberになる
+  const genericsStrArray = repeat<string>('generics', 5);
+  console.log(genericsNumArray) // => [10, 10, 10, 10]
+  console.log(genericsStrArray) // => ['generics', 'generics', 'generics', 'generics']
 };
 
 export default Example;
