35757b3309cfb33e79a74d75d6db25022bdbce6b
y-takagiiii <takagiyuri0712@gmail.com>
2023-09-20 17:44:31 +0900

    fallbackプロパティの挙動を理解しよう

--- 18_nextjs_p2/start/src/pages/020_SG/[id].js
+++ 18_nextjs_p2/start/src/pages/020_SG/[id].js
@@ -1,11 +1,27 @@
+// ファイル名を[]で囲った場合は、リクエストされたパスに応じて動的にページを生成する「ダイナミックルーティング」を行うファイルになる
+import { useRouter } from "next/router"
+
 export default function Page({ id }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <h3>Loading...</h3>
+  }
   return <h3>このページは{id}です。</h3>
 }
 
 export async function getStaticPaths() {
   return {
     paths: [{ params: { id: "1" }}, { params: { id: "2" }}],
-    fallback: false
+    // fallbackオプション: 想定外のパラメータを指定された場合、どういった挙動にするかどうか
+    // pathsプロパティで設定したパラメータ以外にアクセスした場合、404ページを返す
+    // fallback: false
+
+    // pathsプロパティで設定したパラメータ以外にアクセスした場合、サーバー側でgetStaticProps関数を呼び出して動的にページを生成する
+    fallback: true
+
+    // 基本的にはtrueと同じだが、サーバー側で完全なHTMLが生成されるまでHTMLを返さない
+    // つまり、Loading...は表示されず、このページは○ですと表示される
+    // fallback: "blocking",
   };
 }
 
