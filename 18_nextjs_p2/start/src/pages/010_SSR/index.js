57427b074ba1a00a5d58abd03495d05592699272
y-takagiiii <takagiyuri0712@gmail.com>
2023-09-20 15:04:01 +0900

    SSRのgetServerSideProps関数について学ぼう

--- 18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react"
 
-export default function SSR() {
+// 下のgetServerSideProps関数が返すpropsオブジェクトのmessageキーを分割代入で引数として取得
+export default function SSR({ message }) {
   console.log("hello")
+  console.log(message)
   const [state, setState] = useState('bye');
   const val = 0
 
@@ -11,8 +13,28 @@ export default function SSR() {
     // node.jsのログにはuseEffect内のconsole.logは出力されない
     console.log('useEffect')
     window.localStorage.setItem('key', 'val')
+    document.cookie = 'val=0; path=/;'
   }, []);
 
   // SSRではサーバー側で変数に値が格納された後のHTMLをクライアントに渡す
   return <h3>{state}</h3>
+}
+
+// node.js上で実行される関数
+// 引数contextにはリクエストとレスポンスの情報が渡ってくる
+export async function getServerSideProps(context) {
+  // contextからcookie情報を取得することもできる
+  const { cookie } = context.req.headers;
+  console.log('cookie', cookie)
+  console.log("getServerSideProps executed")
+
+  return {
+    // redirectオブジェクトを渡してリダイレクトさせることもできる
+    // redirect: {
+      //   destination: '/',
+      //   permanent: false
+      // },
+    // propsオブジェクトが上のfunction SSRの引数に渡される
+    props: { message: 'From Server Side Props' }
+  }
 }
