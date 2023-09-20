ad5f41828c653b0c1270861c9c7da83d148dea20
y-takagiiii <takagiyuri0712@gmail.com>
2023-09-20 16:54:49 +0900

    getStaticPropsについて学ぼう

--- 18_nextjs_p2/start/src/pages/020_SG/index.js
+++ 18_nextjs_p2/start/src/pages/020_SG/index.js
@@ -1,4 +1,13 @@
 // Static Site Generation
-export default function IndexPage() {
-  return <h3>SG</h3>
+export default function IndexPage({ message }) {
+  return <h3>SG: {message}</h3>
+}
+
+// SGしたいページにpropsを渡す関数
+// getServerSidePropsはnode.js上で実行されるが、
+// getStaticPropsはビルドされる時に実行される
+export async function getStaticProps() {
+  return {
+    props: { message: 'From Static Props' }
+  }
 }
