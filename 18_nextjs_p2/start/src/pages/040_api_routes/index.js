17cb6a529435974421cd2887bd8cb2d13f9a673d
y-takagiiii <takagiyuri0712@gmail.com>
2023-09-21 17:02:11 +0900

    APIを叩いて表示

---
+++ 18_nextjs_p2/start/src/pages/040_api_routes/index.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import ArticleList from "../../components/articleList"
+import Head from "next/head"
+import { useEffect, useState } from "react";
+
+export default function Page() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const ENDPOINT = "/api/articles";
+      const result = await axios.get(ENDPOINT).then(res => res.data);
+      setArticles(result)
+    };
+    getArticles();
+  }, []);
+
+  if(!articles) {
+    return <div>データがありません</div>
+  }
+
+  return (
+    <>
+      <Head>
+        <title>ページ一覧</title>
+      </Head>
+      <h3>フェッチ&SG</h3>
+      <ArticleList list={articles} />
+    </>
+  )
+}
